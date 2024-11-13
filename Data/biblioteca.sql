56a424b421f749be9eaa4e6a7936ea3669c1c589
XimenaG-0 <angelaximena.galeano@gmail.com>
2024-11-13 10:40:12 +0100

    Create biblioteca.sql
    Laboratorio 4 - Crear primera Base de datos, correspondiente al prestamos de libros a estudiantes.

---
+++ Data/biblioteca.sql
@@ -0,0 +1,271 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-11-2024 a las 10:24:23
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autores`
+--
+
+CREATE TABLE `autores` (
+  `ID_Autor` int(11) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Nacionalidad` varchar(50) DEFAULT NULL,
+  `Fecha_Nacimiento` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `autores`
+--
+
+INSERT INTO `autores` (`ID_Autor`, `Nombre`, `Nacionalidad`, `Fecha_Nacimiento`) VALUES
+(1, 'Gabriel García Márquez', 'Colombiano', '1927-03-06'),
+(2, 'J.K. Rowling', 'Británica', '1965-07-31'),
+(3, 'George Orwell', 'Británica', '1903-06-25'),
+(4, 'Gabriel García Márquez', 'Colombiano', '1927-03-06'),
+(5, 'J.K. Rowling', 'Británica', '1965-07-31'),
+(6, 'George Orwell', 'Británica', '1903-06-25');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `ID_Categorias` int(11) NOT NULL,
+  `Nombre_Categoria` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`ID_Categorias`, `Nombre_Categoria`) VALUES
+(1, 'Novela'),
+(2, 'Ciencia Ficción'),
+(3, 'Fantasía'),
+(4, 'Ensayo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `ID_Libros` int(11) NOT NULL,
+  `Titulo_Libros` varchar(200) NOT NULL,
+  `ID_Autor` int(11) DEFAULT NULL,
+  `ID_Categoria` int(11) DEFAULT NULL,
+  `Año_publicacion` int(11) DEFAULT NULL,
+  `Disponible` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`ID_Libros`, `Titulo_Libros`, `ID_Autor`, `ID_Categoria`, `Año_publicacion`, `Disponible`) VALUES
+(1, 'Cien Años de Soledad', 1, 1, 1967, 1),
+(2, 'Harry Potter y la Piedra Filosofal', 2, 3, 1997, 1),
+(3, 'La era de 1984', 3, 2, 1949, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos`
+--
+
+CREATE TABLE `prestamos` (
+  `ID_Prstamo` int(11) NOT NULL,
+  `ID_Usuario` int(11) DEFAULT NULL,
+  `ID_Libro` int(11) DEFAULT NULL,
+  `Fecha_prestamo` date DEFAULT NULL,
+  `Fecha_devolucion` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `prestamos`
+--
+
+INSERT INTO `prestamos` (`ID_Prstamo`, `ID_Usuario`, `ID_Libro`, `Fecha_prestamo`, `Fecha_devolucion`) VALUES
+(1, 1, 1, '2024-08-01', '2024-08-15'),
+(2, 2, 2, '2024-08-02', '2024-08-16');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos_usuairos`
+--
+
+CREATE TABLE `prestamos_usuairos` (
+  `ID_prestamoUsuario` int(11) NOT NULL,
+  `ID_Prestamo` int(11) DEFAULT NULL,
+  `ID_Usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `prestamos_usuairos`
+--
+
+INSERT INTO `prestamos_usuairos` (`ID_prestamoUsuario`, `ID_Prestamo`, `ID_Usuario`) VALUES
+(1, 1, 1),
+(2, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID_Usuario` int(11) NOT NULL,
+  `Nombre_Usuario` varchar(100) NOT NULL,
+  `Direccion` varchar(100) NOT NULL,
+  `Telefono` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`ID_Usuario`, `Nombre_Usuario`, `Direccion`, `Telefono`) VALUES
+(1, 'Carlos Martínez', 'Calle Lun', '123,123456789'),
+(2, 'Lucía Fernández', 'Avenida Sol', '456, 987654321');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `autores`
+--
+ALTER TABLE `autores`
+  ADD PRIMARY KEY (`ID_Autor`);
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`ID_Categorias`);
+
+--
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`ID_Libros`),
+  ADD KEY `ID_Autor` (`ID_Autor`),
+  ADD KEY `ID_Categoria` (`ID_Categoria`);
+
+--
+-- Indices de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD PRIMARY KEY (`ID_Prstamo`),
+  ADD KEY `ID_Libro` (`ID_Libro`),
+  ADD KEY `ID_Usuario` (`ID_Usuario`);
+
+--
+-- Indices de la tabla `prestamos_usuairos`
+--
+ALTER TABLE `prestamos_usuairos`
+  ADD PRIMARY KEY (`ID_prestamoUsuario`),
+  ADD KEY `ID_Prestamo` (`ID_Prestamo`),
+  ADD KEY `ID_Usuario` (`ID_Usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID_Usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `autores`
+--
+ALTER TABLE `autores`
+  MODIFY `ID_Autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `ID_Categorias` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `ID_Libros` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  MODIFY `ID_Prstamo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos_usuairos`
+--
+ALTER TABLE `prestamos_usuairos`
+  MODIFY `ID_prestamoUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID_Usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD CONSTRAINT `libros_ibfk_1` FOREIGN KEY (`ID_Autor`) REFERENCES `autores` (`ID_Autor`),
+  ADD CONSTRAINT `libros_ibfk_2` FOREIGN KEY (`ID_Categoria`) REFERENCES `categorias` (`ID_Categorias`);
+
+--
+-- Filtros para la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`ID_Libro`) REFERENCES `libros` (`ID_Libros`),
+  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`ID_Usuario`) REFERENCES `usuarios` (`ID_Usuario`);
+
+--
+-- Filtros para la tabla `prestamos_usuairos`
+--
+ALTER TABLE `prestamos_usuairos`
+  ADD CONSTRAINT `prestamos_usuairos_ibfk_1` FOREIGN KEY (`ID_Prestamo`) REFERENCES `prestamos` (`ID_Prstamo`),
+  ADD CONSTRAINT `prestamos_usuairos_ibfk_2` FOREIGN KEY (`ID_Usuario`) REFERENCES `usuarios` (`ID_Usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
